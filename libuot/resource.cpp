07d9f679c64e4db62cc488ff558ef45b9e296baf
piotr.onyszczuk1 <piotr.onyszczuk1@gmail.com>
2021-09-10 13:41:38 +0200

    Operators fix

---
+++ libuot/resource.cpp
@@ -0,0 +1,29 @@
+#include "resource.h"
+std::map<Resource, float> operator+(std::map<Resource, float> a, const std::map<Resource, float>& b)
+{
+    for (const auto& p : b)
+    {
+        a[p.first] += p.second;
+    }
+    return a;
+}
+
+std::map<Resource, float>& operator+=(std::map<Resource, float>& a, const std::map<Resource, float>& b)
+{
+    for (const auto& p : b)
+    {
+        a[p.first] += p.second;
+    }
+    return a;
+}
+
+std::map<Resource, float> operator*(std::map<Resource, float> a, const float b)
+{
+    for (auto& p : a)
+    {
+        p.second *= b;
+    }
+    return a;
+}
+
+std::map<Resource, float> operator*(const float b, std::map<Resource, float> a) { return a * b; }
--- libuot/resource.h
+++ libuot/resource.h
@@ -25,31 +25,7 @@ enum class Resource
     Food,  // Food is special
 };
 
-std::map<Resource, float> operator+(std::map<Resource, float> a, const std::map<Resource, float>& b)
-{
-    for (const auto& p : b)
-    {
-        a[p.first] += p.second;
-    }
-    return a;
-}
-
-std::map<Resource, float>& operator+=(std::map<Resource, float>& a, const std::map<Resource, float>& b)
-{
-    for (const auto& p : b)
-    {
-        a[p.first] += p.second;
-    }
-    return a;
-}
-
-std::map<Resource, float> operator*(std::map<Resource, float> a, const float b)
-{
-    for (auto& p : a)
-    {
-        p.second *= b;
-    }
-    return a;
-}
-
-std::map<Resource, float> operator*(const float b, std::map<Resource, float> a) { return a * b; }
+std::map<Resource, float> operator+(std::map<Resource, float> a, const std::map<Resource, float>& b);
+std::map<Resource, float>& operator+=(std::map<Resource, float>& a, const std::map<Resource, float>& b);
+std::map<Resource, float> operator*(std::map<Resource, float> a, const float b);
+std::map<Resource, float> operator*(const float b, std::map<Resource, float> a);
