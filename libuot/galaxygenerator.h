e7a245a8c01f5f389bbf775bfcf0950a2a673e72
Franciszek Jełowicki <franek.jelowicki@gmail.com>
2022-02-03 22:51:41 +0100

    Better galaxy generator

--- libuot/galaxygenerator.h
+++ libuot/galaxygenerator.h
@@ -2,11 +2,26 @@
 
 #include "galaxy.h"
 
-struct GalaxyGeneratorParameters
-{
-    int size;
-    float habitable_planet_chance_multipler;
-};
+#include <random>
 
-Galaxy GenerateGalaxy(const GalaxyGeneratorParameters& parameters);
-Galaxy GenerateGalaxyTest(const GalaxyGeneratorParameters& parameters);
+class GalaxyGenerator
+{
+    const int size;
+    const float habitable_planet_chance_multipler;
+
+    std::random_device dev;
+    std::mt19937 gen;
+
+    Point PointOnCircle(float r);
+    std::vector<Point> GeneratePositionsInSector(const std::vector<Point> &existing_points, int count);
+    void GenerateBlackHoleSector(std::set<std::shared_ptr<SectorObject>> &sector_objects, std::mt19937 &gen);
+    std::shared_ptr<SectorObject> GenerateInhabitable(const Point &pos, InhabitableObject::ObjectType type,
+                                                      std::discrete_distribution<> resources_distribution);
+    std::shared_ptr<SectorObject> GenerateHabitablePlanet(const Point &p);
+
+    std::set<std::shared_ptr<SectorObject>> GenerateSectorObjects();
+
+   public:
+    GalaxyGenerator(int size, float habitable_planet_chance_multipler);
+    Galaxy Generate();
+};
