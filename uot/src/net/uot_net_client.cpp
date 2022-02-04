3b3733add8dcc9f5ef2fed6270e73371bb901c9d
Paweł Grabowski <gprabowski@outlook.com>
2022-02-04 01:28:23 +0100

    Add server synchronised building mechanism, add day counter to queue

--- uot/src/net/uot_net_client.cpp
+++ uot/src/net/uot_net_client.cpp
@@ -173,6 +173,7 @@ void uot_net_client::handle_message(const std::string& data)
                                 _planet->colony->owner = player_ptr;
                                 _planet->colony->population = planet.colony.population;
                                 _planet->colony->buildings = planet.colony.buildings;
+                                player_ptr->owned_colonies[_planet->colony->id] = _planet->colony;
                                 colonies_map.insert(
                                     std::pair<int, std::shared_ptr<Colony>>(_planet->colony->id, _planet->colony));
                             }
@@ -264,8 +265,21 @@ void uot_net_client::handle_message(const std::string& data)
                     }
                 }
 
-                for (const auto& r : resource_data)
-                {
+                for(const auto& a : payload_newturn->buildings_updates) {
+                    state.value->player->owned_colonies[a.colony_id]->building_queue.clear();
+                }
+
+                for(const auto& a : payload_newturn->buildings_updates) {
+                    if(a.days_remaining == 1) {
+                        state.value->player->owned_colonies[a.colony_id]->buildings[a.building_type]++;
+                    } else {
+                        BuildingBuildProgress b{a.building_type, a.upgrade_of};
+                        b.worker_week_units_left = a.days_remaining;
+
+                        state.value->player->owned_colonies[a.colony_id]->building_queue.push_back(b);
+                    }
+                }
+                for (const auto& r : resource_data) {
                     state.value->player->owned_resources[r.first] = r.second;
                 }
 
