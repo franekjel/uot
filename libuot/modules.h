280e233145939e62728b64e5331a832bf0e717d6
michalMilewski-8 <miles98@interia.pl>
2022-02-12 14:04:25 +0100

    clang

--- libuot/modules.h
+++ libuot/modules.h
@@ -50,11 +50,11 @@ struct Module
         Inside,
     } destination;
 
-    float generating_energy = 0.0f;       // reactors only
+    float generating_energy = 0.0f;  // reactors only
     float energy_capacity = 0.0f;    // reactors only
-    float shield_capacity = 0.0f;         // shields only
+    float shield_capacity = 0.0f;    // shields only
     float additional_hp = 0.0f;      // armor only
-    float contruction_speed = 0.0f;       // construction modules only
+    float contruction_speed = 0.0f;  // construction modules only
     float human_capacity = 0.0f;     // quarters only
     std::optional<Weapon> weapon;    // weapons only
     static constexpr float nanobots_hp_regen_amount = 20.0f;
