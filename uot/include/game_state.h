c234092fa68d65beab50295fac328ef7fdd4a0a7
Paweł Grabowski <gprabowski@outlook.com>
2022-03-03 00:33:59 +0100

    Add fleet mutexes + current_fleet data separation

--- uot/include/game_state.h
+++ uot/include/game_state.h
@@ -5,6 +5,7 @@
 #include <atomic>
 #include <optional>
 #include <variant>
+#include <mutex>
 #include <vector>
 #include "../../libuot/sector.h"
 #include "sdl_utilities.h"
@@ -19,6 +20,7 @@ struct position
 
 struct game_state : public uncopiable
 {
+
    protected:
     game_state() {}
     friend singleton<game_state>;
@@ -31,6 +33,11 @@ struct game_state : public uncopiable
     std::map<unsigned int, std::shared_ptr<Ship>> ships;
     std::map<unsigned int, std::shared_ptr<Player>> all_players;
     std::map<unsigned int, std::shared_ptr<Fleet>> enemies_fleet_in_current_sector;
+
+    // i know it's ugly and unsafe, but we hold state references
+    // all over the place, it doesnt matter at this point
+    std::mutex present_fleet_mutex;
+    std::mutex enemy_fleet_mutex;
 };
 
 #endif
